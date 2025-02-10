ac1beb56f7639395cef7cd99d133d0cc0ffda61a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-10 09:32:24 -0300

    Primeira aula de java, 3 exercicios simples

---
+++ src/main/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author labsfiap
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 8;
+        int nota3 = 9;
+        
+        double xmedia = (nota1 + nota2 + nota3) / 3;
+        
+        
+        System.out.println("Exércicio 1: \n A média do aluno é " + xmedia + "\n");
+        
+        int valor = 10;
+        double dezPorcento = 10 * 10 / 100;
+        
+        System.out.println("Exércicio 2: \n A porcentagem de  " + valor + "é: \n: " + dezPorcento + "\n");
+        
+        
+        int a = 15;
+        int b = 20;
+        System.out.println("Exércicio 3 \nA = " + a + " B = " + b);
+        System.out.println("A + B " + "= " + (a+b));
+        System.out.println("A * B " + "= " + (a*b));
+        System.out.println("A / B " + "= " + (a/b));
+        System.out.println("A - B " + "= " +(a-b));
+
+    }
+}
