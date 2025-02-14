63da8e0d8fc67e98592311eba62b5635c52e2547
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2025-02-14 11:42:57 -0300

    aula dia 14/02

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,28 +11,97 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        int nota1 = 10;
-        int nota2 = 8;
-        int nota3 = 9;
-        
-        double xmedia = (nota1 + nota2 + nota3) / 3;
-        
-        
-        System.out.println("Exércicio 1: \n A média do aluno é " + xmedia + "\n");
-        
-        int valor = 10;
-        double dezPorcento = 10 * 10 / 100;
-        
-        System.out.println("Exércicio 2: \n A porcentagem de  " + valor + "é: \n: " + dezPorcento + "\n");
-        
-        
-        int a = 15;
-        int b = 20;
-        System.out.println("Exércicio 3 \nA = " + a + " B = " + b);
-        System.out.println("A + B " + "= " + (a+b));
-        System.out.println("A * B " + "= " + (a*b));
-        System.out.println("A / B " + "= " + (a/b));
-        System.out.println("A - B " + "= " +(a-b));
+        Scanner x = new Scanner(System.in);
+        x.useLocale(Locale.US);
+        // AULA DIA 10/02 - SEGUNDA-FERIRA
 
+        // int nota1 = 10;
+        // int nota2 = 8;
+        // int nota3 = 9;
+        
+        // double xmedia = (nota1 + nota2 + nota3) / 3;
+        
+        
+        // System.out.println("Exércicio 1: \n A média do aluno é " + xmedia + "\n");
+        
+        // int valor = 10;
+        // double dezPorcento = 10 * 10 / 100;
+        
+        // System.out.println("Exércicio 2: \n A porcentagem de  " + valor + "é: \n: " + dezPorcento + "\n");
+        
+        
+        // int a = 15;
+        // int b = 20;
+        // System.out.println("Exércicio 3 \nA = " + a + " B = " + b);
+        // System.out.println("A + B " + "= " + (a+b));
+        // System.out.println("A * B " + "= " + (a*b));
+        // System.out.println("A / B " + "= " + (a/b));
+        // System.out.println("A - B " + "= " +(a-b));
+
+
+        // AULA DIA 14/02 SEXTA-FEIRA
+         // ---- Exercicio apenas para começar a aula ----
+//        int valor;
+//        double valor2;
+//        String nome = "sdssds";
+//
+//
+//        System.out.println("Digite o valor inteiro:");
+//        valor = x.nextInt();
+//
+//        System.out.println("Digite o valor decimal (use ponto, ex: 23.3):");
+//        valor2 = x.nextDouble();
+//
+//        System.out.println("Valor inteiro: " + valor);
+//        System.out.println("Valor decimal: " + valor2);
+//        x.close();
+        // ---- Exercicio 1 ----
+//        DEVERAM EXISTIR AS SEGUINTES  VARIAVEIS INTEIRAS X , Y E Z
+        //SENDO QUE EU NECESSITO  DAS SEGUINTES RESPOSTAS
+        //Y-X      Y+Z        Y / X
+//        double xis;
+//        double y;
+//        double z;
+//
+//        System.out.println("Digite o valor de xis: ");
+//        xis = x.nextDouble();
+//        System.out.println("Digite o valor de y: ");
+//        y = x.nextDouble();
+//        System.out.println("Digite o valor de z: ");
+//        z = x.nextDouble();
+//        System.out.println("O resultado da conta " + y + " - " + xis + ": " + (y - xis));;
+//        System.out.println("O resultado da conta " + y + " + " + z + ": " + (y + z));;
+//        System.out.println("O resultado da conta " + y + " / " + xis + ": " + (y / z));;
+//        x.close();
+        // ---- Exercicio 2 ---- 
+//        CONCATENE AS SEGUINTES FRASES E VALORES ABAIXO :
+        //" ISSO É UM TESTE, "      "  PORTANTO A NUMERAÇÃO SEGUINTE  É 2 + 2 =    4 "
+//        String teste = " ISSO É UM TESTE, ";
+//        String numeracao = "  PORTANTO A NUMERAÇÃO SEGUINTE  É 2 + 2 =    4";
+//        System.out.println(teste + numeracao);
+//        x.close();
+        // ---- Exercicio 3 ---- 
+//        double resposta;
+//        int n1 = 0;
+//        int n2 = 0;
+//        System.out.println("Caso nao escolha um valor para n1  valera 0, n1: ");
+//        n1 = x.nextInt();
+//        System.out.println("Caso nao escolha um valor para n2  valera 0, n2: ");
+//        n2 = x.nextInt();
+//
+//        resposta = n1 / n2;
+//        System.out.println(resposta);
+//        x.close();
+        // ---- Exercicio 4 ---- 
+//        double salario;
+//        System.out.println("Salario: ");
+//        salario = x.nextDouble();
+//        double valeTransporte = (salario * 6) / 100;
+//        double impostoDeRenda = (salario * 2) / 100;
+//        double planoDeSaude = (salario * 12) / 100;
+//        double somaDescontos = valeTransporte + impostoDeRenda + planoDeSaude;
+//        double salarioLiquido = salario - somaDescontos;
+//        System.out.println("O salario bruto e: " + salario + ", e os descontos foram " + valeTransporte + " de V.T " + impostoDeRenda + " de IR " + planoDeSaude + " de plano de saude, e o salario liquido foi de " + salarioLiquido);
+//        x.close();
     }
 }
